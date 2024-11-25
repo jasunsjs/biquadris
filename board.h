7915a9cb3dfdbb7dd6984e343f3a84c76aa3cd39
Ryan Zhou <110564801+RyanZCode@users.noreply.github.com>
2024-11-25 16:58:34 -0500

    Update board.h - adjusted spacing

--- board.h
+++ board.h
@@ -3,15 +3,12 @@
 #include "decorator.h"
 
 class Board {
-    private:
-        int width, height;
-        Decorator* picture;
-    
-    public:
-        char getState(int x, int y);
-        void removeLayer();
-        void checkBoard();
-        
+    int width, height;
+    Decorator* picture;
+  public:
+    char getState(int x, int y);
+    void removeLayer();
+    void checkBoard();
 };
 
 #endif
