792f8f045938f9f74df077782c83acf9d9682b0f
Ryan Zhou <110564801+RyanZCode@users.noreply.github.com>
2024-11-25 22:55:36 -0500

    Update board.h - added Decorator* to constructor

--- board.h
+++ board.h
@@ -6,7 +6,7 @@ class Board {
     int rows, cols;
     Decorator* picture;
   public:
-    Board(int rows = 15, int cols = 11);
+    Board(int rows = 15, int cols = 11, Decorator* picture);
     char getState(int x, int y) const override;
     void removeLayer();
     void checkBoard();
