43247674938c11fdc07193f6d90e388a4365250f
Jason Sun <jasonsunjishen@gmail.com>
2024-11-26 01:17:40 -0500

    asdasdasd

---
+++ tblock.h
@@ -0,0 +1,13 @@
+#ifndef TBLOCK_H
+#define TBLOCK_H
+
+#include "decorator.h"
+
+class TBlock: public Decorator {
+  public:
+    void rotateClockwise() override;
+    void rotateCounterClockwise() override;
+    char charAt(int x, int y) override;
+};
+
+#endif
