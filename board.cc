2b8995e608a4c1a02323f3b58c8a0f6d2238c469
Ryan Zhou <110564801+RyanZCode@users.noreply.github.com>
2024-11-25 22:55:11 -0500

    Update board.cc - added constructor

--- board.cc
+++ board.cc
@@ -1,5 +1,7 @@
 #include "board.h"
 
+Board::Board(int rows, int cols, Decorator* picture) : rows{rows}, cols{cols}, picture{picture} {}
+
 char Board::getState(int x, int y) const {
     return picture->charAt(x, y);
 }
