7c8c698095a9c7db393413f78a4d708a55c09c1f
swang142 <to.stanley.wang4130@gmail.com>
2024-11-27 17:50:43 -0500

    textobserver impl

--- textobserver.cc
+++ textobserver.cc
@@ -11,15 +11,30 @@ void TextObserver::update(){
     out << "Score: " << subject->getScore() << std::endl;
 
 
-    out << "------------" << std::endl;
+    out << "-------------" << std::endl;
 
     //updates state of each coord 
     for(int r = 0; r < height; r++){
+        out << '|';
         for(int c = 0; c < width; c++){
             out << subject->getState(r, c);
         }
+        out << '|' << std::endl;
+    } 
+
+     out << "-------------" << std::endl;
+     out << "Next Block: " << std::endl;
+
+    char nextBlock = subject->getNextBlock();
+    auto it = blockShapes.find(nextBlock);
+
+    if (it != blockShapes.end()) {
+        for (const std::string& line : it->second) {
+            out << line << std::endl;
+        }
         out << std::endl;
+    } else {
+        out << "    " << std::endl;
     }
 
-     out << "------------" << std::endl;
 }
--- textobserver.h
+++ textobserver.h
@@ -4,9 +4,21 @@
 #include "observer.h"
 #include "window.h"
 #include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
 
 class TextObserver : public Observer {
     std::ostream& out;
+    const std::unordered_map<char, std::vector<std::string>> blockShapes = {
+        {'I', {"IIII"}},          // I-block
+        {'J', {"J  ", "JJJ"}},    // J-block
+        {'L', {"  L", "LLL"}},    // L-block
+        {'O', {"OO", "OO"}},      // O-block
+        {'S', {" SS", "SS "}},    // S-block
+        {'Z', {"ZZ ", " ZZ"}},    // Z-block
+        {'T', {"TTT", " T "}}     // T-block
+    };
 
     public:
         TextObserver(Board* s, int w, int h, std::ostream& o);
