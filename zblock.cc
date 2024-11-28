31c69254ecb600fd993e82f518762cdc114bd118
Jason Sun <jasonsunjishen@gmail.com>
2024-11-27 20:04:26 -0500

    zblock finished

---
+++ zblock.cc
@@ -0,0 +1,51 @@
+#include "zblock.h"
+
+using namespace std;
+
+
+void ZBlock::rotateClockwise() {
+    vector<pair<int, int>> newCoords = coords;
+    pair<int, int> newBottomLeft = bottomLeft;
+
+    transpose(newCoords, newBottomLeft);
+    
+    // Set new bottom left anchor and reverse cols
+    if (state == RotationState::Default) {
+        newBottomLeft.first -= 1;
+        newBottomLeft.second += 2;
+
+        // Change state
+        state = RotationState::Rotated90;   
+    } else {
+        newBottomLeft.first -= 2;
+        newBottomLeft.second += 1;
+
+        // Change state
+        state = RotationState::Default; 
+    }
+
+    reposition(newCoords, newBottomLeft);
+
+    if (isValid(newCoords)) {
+        coords = newCoords;
+    }
+}
+
+void ZBlock::rotateCounterClockwise() {
+    for (int i = 0; i < 3; ++i) {
+        rotateClockwise();
+    }
+}
+
+char ZBlock::charAt(int x, int y) const {
+
+    // Check if within block
+    for (pair<int, int> p : coords) {
+        if (p.first == x && p.second == y) {
+            return symbol;
+        }
+    }
+
+    // Else call next decorator's charAt()
+    return component->getState(x, y);
+}
