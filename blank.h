65a3b5fc7269a099a2912f28979066998e98dc49
Jason Sun <jasonsunjishen@gmail.com>
2024-11-27 23:54:12 -0500

    added ctor for blank

--- blank.h
+++ blank.h
@@ -5,6 +5,10 @@
 
 class Blank: public Decorator {
   public:
+    Blank(Board *component, 
+           std::vector<std::pair<int, int>> coords = {}, 
+           char symbol = ' ', 
+           int generatedLevel);
     char charAt(int x, int y) const override;
 };
 
